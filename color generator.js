b481691de231a1e11fedeb1a6b9b26bfc9a97a18
poojithgowd <151959723+poojithgowd@users.noreply.github.com>
2024-01-03 17:24:43 +0530

    Update color generator.js

--- color generator.js
+++ color generator.js
@@ -1,8 +1,10 @@
-const colors = ['red', 'blue', 'green', 'yellow', 'gray', 'pink', 'black'];
-
-function changeColor() {
-    const pickColor = Math.floor(Math.random() * 7);
-    const col = colors[pickColor];
-    const button = document.getElementById('id1');
-    button.style.background = col;
+const redValues = [0, 51, 102, 153, 204, 255];
+const greenValues = [0, 51, 102, 153, 204, 255];
+const blueValues = [0, 51, 102, 153, 204, 255];
+function generateColor() {
+  const randomRed = redValues[Math.floor(Math.random() * redValues.length)];
+  const randomGreen = greenValues[Math.floor(Math.random() * greenValues.length)];
+  const randomBlue = blueValues[Math.floor(Math.random() * blueValues.length)];
+  const button = document.getElementById('id1');
+  button.style.background = `rgb(${randomRed},${randomGreen},${randomBlue})`;
 }
